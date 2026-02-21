8a33e59efe1cea95ecffa6642deb01168423faaf
weird94 <zhang9748@foxmail.com>
2026-02-22 06:47:03 +0800

    feat: Export the new constants module.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 export * from './types'
 export * from './id'
+export * from './const'
 export * from './hooks/use-content-rpc-service'
 export * from './hooks/use-background-rpc-service'
 
 // 统一 API
 export * from './host'
 export * from './unified-client'
-
